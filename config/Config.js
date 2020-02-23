3b0fa423ae135aedd548ae6f3864e8a6f82a1f82
wajdibenhelal <wajdibenhelal52@live.fr>
2020-02-23 22:59:32 +0100

    Api Firebase for register and login

---
+++ config/Config.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase";
+
+var config = {
+    apiKey: "AIzaSyCo8K7mmdvm85OUfXO1c6NFz8d4Ar9fuT0",
+    authDomain: "reactnative-2c39d.firebaseapp.com",
+    databaseURL: "https://reactnative-2c39d.firebaseio.com",
+    projectId: "reactnative-2c39d",
+    storageBucket: "reactnative-2c39d.appspot.com",
+    messagingSenderId: "963721597161",
+    appId: "1:963721597161:web:62d7a156e6a45fc18ea078",
+    measurementId: "G-P9ZHDRX8V5"
+};
+firebase.initializeApp(config);
